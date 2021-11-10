5a51c60cb902ee8c244e92e776250cda7331a6ae
Abdulmajeed Kutaibani <Abodymeo@yahoo.com>
2021-11-10 18:20:01 +0300

    added initial styled components

--- product-page/src/App.js
+++ product-page/src/App.js
@@ -1,5 +1,21 @@
+import { ThemeProvider } from 'styled-components';
+import { Container } from './Components/Style/Container.style';
+import { GlobalStyles } from './Components/Style/Global';
+const theme = {
+  colors: {
+    mainColor: '#ff7d1b',
+    secondaryColor: '#ffa05a',
+    textColor: '#1e1e26',
+    subColor: '#94979e',
+  },
+};
 function App() {
-  return <div className='App'></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Container>iwuehgiwerhghieehr</Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ product-page/src/Components/Style/Global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+*{
+    box-sizing: border-box;
+  
+}
+
+body{
+    margin: 0;
+    font-family:'Kumbh Sans', sans-serif;
+}
+
+`;
